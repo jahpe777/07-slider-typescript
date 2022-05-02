5b3755573d606d86bf304a000dc31909aae1e5e0
James Higgs <james.y.higgs@gmail.com>
2022-05-01 17:53:15 -0700

    220501

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+import { People } from './model';
+
+const App: React.FC = () => {
+  const [people, setPeople] = useState<People[]>(data);
+  const [index, setIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  });
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev">
+          <FiChevronLeft onClick={() => setIndex(index - 1)} />
+        </button>
+        <button className="next">
+          <FiChevronRight onClick={() => setIndex(index + 1)} />
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default App;
---
+++ src/model.ts
@@ -0,0 +1,7 @@
+export interface People {
+  id: number;
+  image: string;
+  name: string;
+  title: string;
+  quote: string;
+}
